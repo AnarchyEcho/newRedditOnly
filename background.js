0b185ee908752912c80267efb2fa7ca4e1a59781
AnarchyEcho <anarchytrex@hotmail.com>
2024-11-23 04:51:20 +0100

    fixed infinite redirect on short share links

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
-const pattern = '*://www.reddit.com/*'
+const host = 'https://new.reddit.com'
 
 function redirect(requestDetails) {
   console.log(`Redirecting: ${requestDetails.url}`);
 
-  return {
-    redirectUrl: requestDetails.url.replace('www.', 'new.'),
-  };
+  return { redirectUrl: host + requestDetails.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1] };
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
   redirect,
   {
-    urls: [pattern],
+    urls: [
+      '*://reddit.com/',
+      '*://reddit.com/r/*/comments/*',
+      '*://www.reddit.com/',
+      '*://www.reddit.com/r/*/comments/*',
+    ],
   },
   ['blocking']
 );
